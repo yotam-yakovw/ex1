6838d884b2bff621591a3fb3bc0fe10da5a914cb
CSI-BIU <csi.biu2024@gmail.com>
2024-10-27 01:41:25 +0200

    Create EvenOdd.c

---
+++ EvenOdd.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+
+int main() {
+  // Scan a number
+
+  // If the number is even - print 1, else - print 0.
+  // REMIDER : YOU CANT USE ANY CONTROL FLOW OPERATIONS, ONLY BITWISE.
+  
+  return 0;
+}
