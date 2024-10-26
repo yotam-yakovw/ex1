adeca3ed21a6a9c17c40227be6209f492b4881e6
CSI-BIU <csi.biu2024@gmail.com>
2024-10-27 01:27:10 +0200

    Create SetBit.c

---
+++ SetBit.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main () {
+  // Set Bit:
+  // Scan a number and a position
+  
+  // Make sure the bit in this position is "on"
+  // Print the output
+  
+  return 0;
+}
