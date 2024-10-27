87496d15f61f5bba3b967bebe79fbf871481dfc4
CSI-BIU <csi.biu2024@gmail.com>
2024-10-28 01:02:56 +0200

    Create ex1.c

---
+++ ex1.c
@@ -0,0 +1,41 @@
+// your name and ID
+#include <stdio.h>
+
+// REMIDER : YOU CANT USE ANY CONTROL FLOW OPERATIONS OR FUNCTIONS, ONLY BITWISE.
+
+int main() {
+  
+  // What bit
+  printf("What bit:\n");
+  /*Scan a number and a position
+  Print what is the bit in this position. */
+  
+  // Set bit
+  printf("\nSet bit:\n");
+  /* Scan a number and a position
+  Make sure the bit in this position is "on"
+  Print the output
+  Now make sure it's "off"
+  Print the output */
+
+  // Toggle bit
+  printf("\nToggle bit:\n");
+  /* Scan a number and a position
+  Toggle the bit in this position
+  Print the new number */
+  
+  // Even - Odd
+  printf("\nEven - Odd:\n");
+  /* Scan a number
+  If the number is even - print 1, else - print 0. */
+  
+  // 3, 5, 7, 11
+  printf("\n3, 5, 7, 11:\n");
+  /* Scan two numbers in octadecimal base
+  sum them up and print the result in hexadecimal base
+  Print only 4 bits, in positions: 3,5,7,11 in the result. */
+
+  printf("Bye!:\n");
+  
+  return 0;
+}
