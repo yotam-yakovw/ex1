52087aff8616e7ae02ed8b4276c04d263c3f3f01
CSI-BIU <csi.biu2024@gmail.com>
2024-11-13 17:07:51 +0200

    Update ex1.c

--- ex1.c
+++ ex1.c
@@ -1,4 +1,8 @@
-// your name and ID
+/******************
+Name:
+ID:
+Assignment:
+*******************/
 #include <stdio.h>
 
 // REMIDER : YOU CANT USE ANY CONTROL FLOW OPERATIONS OR FUNCTIONS, ONLY BITWISE.
@@ -7,31 +11,31 @@ int main() {
   
   // What bit
   printf("What bit:\n");
-  /*Scan a number and a position
-  Print what is the bit in this position. */
+  /*Scan two integers (representing number and a position)
+  Print the bit in this position. */
   
   // Set bit
   printf("\nSet bit:\n");
-  /* Scan a number and a position
-  Make sure the bit in this position is "on"
+  /*Scan two integers (representing number and a position)
+  Make sure the bit in this position is "on" (equal to 1)
   Print the output
-  Now make sure it's "off"
+  Now make sure it's "off" (equal to 0)
   Print the output */
 
   // Toggle bit
   printf("\nToggle bit:\n");
-  /* Scan a number and a position
+  /*Scan two integers (representing number and a position)
   Toggle the bit in this position
   Print the new number */
   
   // Even - Odd
   printf("\nEven - Odd:\n");
-  /* Scan a number
+  /* Scan an integer
   If the number is even - print 1, else - print 0. */
   
   // 3, 5, 7, 11
   printf("\n3, 5, 7, 11:\n");
-  /* Scan two numbers in octadecimal base
+  /* Scan two integers in octal base
   sum them up and print the result in hexadecimal base
   Print only 4 bits, in positions: 3,5,7,11 in the result. */
 
